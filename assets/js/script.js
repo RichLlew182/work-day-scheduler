73ea37ea2c59a6dc1e4ed17edd8f9903f2c5e668
Richard Llewellyn <richard.lee.llewellyn@gmail.com>
2024-01-05 19:40:49 +0000

    added custom attributes to each timeblock that shows the 24 hour value of that timeblock, which I have then set to a variable called dataTime in a for loop

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,17 @@ currentDay.text(dayjs().format('dddd MMMM YYYY'))
 
 // TODO: Color-code each time block based on past, present, and future when the time block is viewed.
 
+var currentTime = dayjs().format('H');
+console.log(currentTime + 'PM');
+
+var timeArray = $('.time-block');
+
+for (let i = 0; i < timeArray.length; i++) {
+  dataTime = timeArray[i].dataset.time;
+  console.log(dataTime);
+}
+
+
 // TODO: Allow a user to enter an event when they click a time block
 
 // TODO: Save the event in local storage when the save button is clicked in that time block.
