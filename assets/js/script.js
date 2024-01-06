7621ec734c97278a5697e78a77ae9366b0154c98
Richard Llewellyn <richard.lee.llewellyn@gmail.com>
2024-01-06 16:06:50 +0000

    amended the css of past event to have greyed out text, and removed some previous js code that was commented out

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 //  Display the current day at the top of the calender when a user opens the planner.
 
 var currentDay = $('#currentDay');
-currentDay.text(dayjs().format('dddd MMMM  D[th] YYYY'))
+currentDay.text(dayjs().format('dddd D[th] MMMM YYYY'))
 
 //  Present time blocks for standard business hours when the user scrolls down.
 // This has been hardcoded in the HTML
@@ -9,7 +9,6 @@ currentDay.text(dayjs().format('dddd MMMM  D[th] YYYY'))
 //  Color-code each time block based on past, present, and future when the time block is viewed.
 
 var currentTime = dayjs().format('H');
-// console.log(currentTime + 'PM');
 
 var timeBlockInput = $('.time-block textarea');
 
@@ -17,7 +16,6 @@ $.each(timeBlockInput, function (i, value) {
 
   var dataTime = (parseInt(value.dataset.time));
   var dataInputs = timeBlockInput[i];
-  // console.log(`The current time is ${currentTime}`)
   
   if (dataTime > currentTime) {
     dataInputs.classList.add('future')
@@ -26,52 +24,14 @@ $.each(timeBlockInput, function (i, value) {
   } else if (dataTime < currentTime) {
     dataInputs.classList.add('past')
   }
-
   
 })
 
 
-
-// for (var j = 0; j < timeBlockInput.length; j ++) {
-
-//   timeBlockInput[j].value = "Test-" + [j];
-//   var timeBlockValue = timeBlockInput[j].value
-//   console.log(timeBlockValue);
-
-  
-//   var timeAndValue = {
-//     time: timeBlockInput[j].name,
-//     value: timeBlockValue,
-//   }
-  
-//   existingTimeBlock.push(timeAndValue)
-//   localStorage.setItem("TimeAndValue", JSON.stringify(existingTimeBlock));
-  
-// }
-  
-// var button = document.querySelector("#nineAM > button")
-
-// button.addEventListener("click", function (e) {
-//   e.preventDefault();
-//   console.log(e);
-//   console.log(button.previousElementSibling.children[0].value);
-
-
-//   var timeAndValue = {
-//     time: timeBlockInput[0].name,
-//     value: timeBlockInput[0].value
-//   }
-
-//   existingTimeBlock.push(timeAndValue)
-//   localStorage.setItem("TimeAndValue", JSON.stringify(existingTimeBlock));
-
-// })
-
 // Variable that is either assigned the value of an empty array or the local storage values from the save button function below
-var existingTimeBlock = JSON.parse(localStorage.getItem('TimeAndValue')) || [];
+var existingTimeBlock = JSON.parse(localStorage.getItem('TimeAndAppointment')) || [];
 
 //  Allow a user to enter an event when they click a time block
-
 // Save the event in local storage when the save button is clicked in that time block.
 
 var saveButtons = $('.saveBtn');
@@ -80,15 +40,15 @@ $.each(saveButtons, function (i, button) {
   $(button).on('click', function (e) {
     e.preventDefault();
     var inputTime = $(button).prev().children().eq(0).attr('data-time');
-    var inputValue = $(button).prev().children().eq(0).val();
+    var inputAppointment = $(button).prev().children().eq(0).val();
     
-    var timeAndValue = {
+    var timeAndAppointment = {
       time: inputTime,
-      value: inputValue
+      appointment: inputAppointment
     }
     
-    existingTimeBlock.push(timeAndValue)
-    localStorage.setItem("TimeAndValue", JSON.stringify(existingTimeBlock));
+    existingTimeBlock.push(timeAndAppointment);
+    localStorage.setItem("TimeAndAppointment", JSON.stringify(existingTimeBlock));
     
   })
   
@@ -106,7 +66,7 @@ if (existingTimeBlock.length !== 0) {
       var existingTimeInt = existingTimeBlock[j].time
       
       if (dataTimeInt === existingTimeInt) {
-        this.value = existingTimeBlock[j].value
+        this.value = existingTimeBlock[j].appointment
       }
     }
   })
